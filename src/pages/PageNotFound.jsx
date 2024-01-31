330d41ebe60f41614c645bbd36eec8b8dd753f19
AzamatIkromovDev <AzamatIkromovDev@gmail.com>
2024-01-31 18:51:34 +0500

    added NAVIGATION

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav.jsx";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <PageNav />
+      <h1>Page Not Found </h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
